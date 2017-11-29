6129fa9dcd4b839a76f19977f534daec07f15252
Jessie Pullaro <jessiepullaro@Jessies-MacBook-Air.local>
2017-11-29 00:53:18 -0500

    Main activity plus some other things

---
+++ app/src/main/java/com/example/jessiepullaro/softwareengineering/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.jessiepullaro.softwareengineering;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+/**
+ * Created by jessiepullaro on 11/29/17.
+ */
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstancesBundle)
+    {
+        super.onCreate(savedInstancesBundle);
+        setContentView(R.layout.activity_main);
+
+    }
+
+//    public void goToMarketplace(View view)
+//    {
+//        startActivity(MarketPlaceActivity);
+//    }
+//
+//
+//    public void goToInvent(View view)
+//    {
+//        startActivity(MarketPlaceActivity);
+//    }
+//
+//    public void goToGame(View view)
+//    {
+//        startActivity(TapActivity);
+//    }
+}
