2be388a3af08b9e2c9fa91b3dd1543fa8998c77f
Slatinator <slater.colteryahn@gmail.com>
2017-11-29 01:00:44 -0500

    MarketPlace Activity

---
+++ app/src/main/java/com/example/jessiepullaro/softwareengineering/MarketPlaceActivity.java
@@ -0,0 +1,77 @@
+package com.example.jessiepullaro.softwareengineering;
+
+/**
+ * Created by slatercolteryahn on 11/28/17.
+ */
+
+public class MarketPlaceActivity {
+
+    /*
+
+    public Integer house;
+    public Integer car;
+    public Integer bread;
+    public Integer water;
+    public Integer chicken;
+    public Integer spicyEnchilada;
+    public Integer pubSub;
+
+     private void buyHouse(){
+
+          numCoins--;
+          house++;
+          PlaceHouseIntoDataBase();
+
+     }
+
+     buyCar()
+     {
+
+          numCoins--;
+          car++;
+          PlaceCarIntoDataBase();
+
+     }
+
+     buyBread()
+     {
+
+          numCoins--;
+          bread++;
+          PlaceBreadIntoDataBase();
+     }
+     buyWater()
+     {
+
+          numCoins--;
+          water++;
+          PlaceWaterIntoDataBase();
+
+     }
+     buyChicken()
+     {
+
+          numCoins--;
+          chicken++;
+          PlaceChickenIntoDataBase();
+     }
+     buySpicyEnchilada ()
+     {
+
+          numCoins--;
+          spicyEnchilada++;
+          PlaceSpicyEnchiladaIntoDataBase();
+     }
+     buyPubSub()
+     {
+
+          numCoins--;
+          pubSub++;
+          PlacePubSubIntoDataBase();
+     }
+
+
+
+
+    */
+}
